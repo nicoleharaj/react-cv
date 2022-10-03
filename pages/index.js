a30c8e8a3608bedffda189be94acedce87432b49
Nicole Haraj <nicoleharaj@outlook.com>
2022-10-03 09:52:09 -0400

    configure Tailwind

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
 import Head from 'next/head'
-import Image from 'next/image'
 
 export default function Home() {
   return (
     <div>
       <Head>
         <title>Nicole Haraj</title>
-        <meta name="description" content="Portfolio site" />
+        <meta name="description" content="Nicole's portfolio site" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
