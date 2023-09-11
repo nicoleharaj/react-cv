e621b94440ba4b45f5a28819d6e92972c2d4c2a8
Nicole Haraj <nicoleharaj@outlook.com>
2023-09-11 19:05:26 -0400

    Update requirements

--- src/env.mjs
+++ src/env.mjs
@@ -7,7 +7,6 @@ export const env = createEnv({
    * isn't built with invalid env vars.
    */
   server: {
-    DATABASE_URL: z.string().url(),
     NODE_ENV: z.enum(["development", "test", "production"]),
     // Add `.min(1) on ID and SECRET if you want to make sure they're not empty
   },
@@ -26,7 +25,6 @@ export const env = createEnv({
    * middlewares) or client-side so we need to destruct manually.
    */
   runtimeEnv: {
-    DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
   },
   /**
