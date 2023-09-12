5c636964c7f94d80e2a3add4acde10bffa6dc599
Nicole Haraj <nicoleharaj@outlook.com>
2023-09-12 00:39:05 -0400

    Fix API handler

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,10 +1,12 @@
 import { createNextApiHandler } from "@trpc/server/adapters/next";
 import { env } from "~/env.mjs";
 import { appRouter } from "~/server/api/root";
+import { createTRPCContext } from "~/server/api/trpc";
 
 // export API handler
 export default createNextApiHandler({
   router: appRouter,
+  createContext: createTRPCContext,
   onError:
     env.NODE_ENV === "development"
       ? ({ path, error }) => {
